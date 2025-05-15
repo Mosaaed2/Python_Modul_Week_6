85c646a50e1634aa8104a83f3733a3da7d6af3f1
Mosaaed Sajer <musaaedzaid8@gmail.com>
2025-05-15 22:34:11 +0200

    m

---
+++ Mosaaed/main.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+# Removed invalid line
+
+from Mosaaed.ui.app_ui import TaskApp
+
+
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = TaskApp(root)
+    root.mainloop()
